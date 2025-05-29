7b68dd7a1b6cf897e1f2b4d7da83ddb91395bd11
Bruno Prado <bwprado@gmail.com>
2025-05-29 09:59:48 -0300

    fix: enable startAllSession in config.ts
    - Changed startAllSession from false to true to allow all sessions to start by default.

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export default {
   port: '21465',
   deviceName: 'CEMARC',
   poweredBy: 'Work In Progress - Dev',
-  startAllSession: false,
+  startAllSession: true,
   tokenStoreType: 'file',
   maxListeners: 15,
   customUserDataDir: './userDataDir/',
